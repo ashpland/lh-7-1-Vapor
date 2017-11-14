daed9f401653e875485194fc12c9fa3aacac2bed
Andrew Shopland <hi@ashp.land>
2017-11-13 20:26:48 -0800

    gonna scale back the data structure

--- Sources/App/Store.swift
+++ Sources/App/Store.swift
@@ -5,14 +5,36 @@
 //  Created by Andrew on 2017-11-13.
 //
 
-class Store {
+import Foundation
+
+class Store : NSObject, NSCoding {
+    
+    
     static let sharedInstance = Store()
     
-    var internalStore = [String:String]()
+    private var internalStore = [String:String]()
     
-    init() {
-       
+    override init() {
+        
     }
+    
+    convenience init(withDictionary: [String:String]) {
+        self.init()
+        self.internalStore = withDictionary
+    }
+    
+    
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(internalStore, forKey: "internalStore")
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let internalStore = aDecoder.decodeObject(forKey: "internalStore") as? [String:String]
+            else {return nil}
+        self.init(withDictionary: internalStore)
+    }
+    
     
     func add(_ inputDictionary:[String:Any]) -> Int? {
         if let sanitizedDictionary = inputDictionary.filter(ifString) as? [String : String] {
